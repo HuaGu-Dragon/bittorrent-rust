d3b78765fbbf887bceda72fa3bab138ac79df001
俺不叫化咕龙 <1801943622@qq.com>
2025-08-19 16:43:09 +0800

    Add initial project files and implement bencoded value decoding functionality

---
+++ README.md
@@ -0,0 +1,25 @@
+# Bittorrent build by rust
+
+* todo
+
+* [ ] Decode bencoded string
+
+* [ ] Decode bencoded integers
+
+* [ ] Decode bencoded lists
+
+* [ ] Decode bencoded dictionary
+
+* [ ] Parse torrent file
+
+* [ ] Calculate info hash
+
+* [ ] Piece hashes
+
+* [ ] Discover peers
+
+* [ ] Peer handshake
+
+* [ ] Download a piece
+
+* [ ] Download the whole file
---
+++ src/main.rs
@@ -0,0 +1,28 @@
+use serde_json;
+use std::env;
+
+fn decode_bencoded_value(encoded_value: &str) -> serde_json::Value {
+    if encoded_value.chars().next().unwrap().is_digit(10) {
+        // Example: "5:hello" -> "hello"
+        let colon_index = encoded_value.find(':').unwrap();
+        let number_string = &encoded_value[..colon_index];
+        let number = number_string.parse::<usize>().unwrap();
+        let string = &encoded_value[colon_index + 1..colon_index + 1 + number];
+        return serde_json::Value::String(string.to_string());
+    } else {
+        panic!("Unhandled encoded value: {}", encoded_value)
+    }
+}
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    let command = &args[1];
+
+    if command == "decode" {
+        let encoded_value = &args[2];
+        let decoded_value = decode_bencoded_value(encoded_value);
+        println!("{}", decoded_value.to_string());
+    } else {
+        println!("unknown command: {}", args[1])
+    }
+}
